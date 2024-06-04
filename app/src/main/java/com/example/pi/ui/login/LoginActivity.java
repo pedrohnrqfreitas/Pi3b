1ee5138d6a595e276bce0e43c350c3572c632646
pedro.leitao <pedro.leitao@sicoob.com.br>
2024-06-04 17:22:21 -0300

    fix

--- app/src/main/java/com/example/pi/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/pi/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pi.MainActivity;
 import com.example.pi.databinding.FragmetLoginBinding;
+import com.google.android.material.textfield.TextInputLayout;
 
 
 public class LoginActivity extends AppCompatActivity {
